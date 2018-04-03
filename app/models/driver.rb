7e854077da4484b6115498f73848c175f26a27ca
Kat <kat.eastman@gmail.com>
2018-04-03 13:08:57 -0700

    driver#show displays required information correctly.

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -5,4 +5,29 @@ class Driver < ApplicationRecord
 
   validates :name, presence: true
   validates :vin, presence: true
+
+  def total_earnings
+    subtotal = 0
+    fee = 1.85
+    self.trips.each do |trip|
+      subtotal += trip.cost
+    end
+    subtotal /= 100
+    total = (subtotal - fee) * 0.8
+
+    return total.round(2)
+  end
+
+  def average_rating
+    rating = 0.0
+    denominator = self.trips.count
+
+    self.trips.each do |trip|
+      rating += trip.rating
+    end
+    average = rating / denominator
+
+    return average
+  end
+
 end

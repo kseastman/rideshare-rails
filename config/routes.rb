c7f372596a31cbf374c323da8133faacaaaf1fe7
Kat <kat.eastman@gmail.com>
2018-04-02 17:08:57 -0700

    added routes for driver and passenger

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,26 @@
 Rails.application.routes.draw do
-  get 'drivers/index'
+# Drivers
+  get '/drivers', to: 'drivers#index', as: 'drivers'
+  get '/drivers/new', to: 'drivers#new', as: 'new_driver'
+  post '/drivers', to: 'drivers#create'
 
-  get 'passengers/index'
+  get '/drivers/:id', to: 'drivers#show', as: 'driver'
+  get '/drivers/:id/edit', to: 'drivers#edit', as: 'edit_driver'
+  patch '/drivers/:id', to: 'drivers#update'
+  delete '/drivers/:id', to: 'drivers#destroy'
 
-  get 'trips/index'
+# Passengers
+  get '/passengers', to: 'passengers#index', as: 'passengers'
+  get '/passengers/new', to: 'passengers#new', as: 'new_passenger'
+  post '/passengers', to: 'passengers#create'
 
+  get '/passengers/:id', to: 'passengers#show', as: 'passenger'
+  get '/passengers/:id/edit', to: 'passengers#edit', as: 'edit_passenger'
+  patch '/passengers/:id', to: 'passengers#update'
+  delete '/passengers/:id', to: 'passengers#destroy'
+
+
+# Trips
   get '/trips', to: 'trips#index', as: 'trips'
   get '/trips/new', to: 'trips#new', as: 'new_trip'
   post '/trips', to: 'trips#create'

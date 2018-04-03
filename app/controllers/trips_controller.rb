ae363dba9c9d13c9da1f3d025fdf445c5dc59ae2
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-04-02 17:02:42 -0700

    Added CRUD methods to Trips controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,46 @@
 class TripsController < ApplicationController
+  def index
+    @trips = Trip.all
+  end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    trip = Trip.new(trip_params)
+
+    if trip.save
+      redirect_to trips_path
+    end
+  end
+
+  def show
+    @trip = Trip.find(params[:id])
+  end
+
+  def edit
+    @trip = Trip.find(params[:id])
+  end
+
+  def update
+    trip = Trip.find(params[:id])
+    trip.assign_attributes(trip_params)
+
+    if trip.save
+      redirect_to trip_path(trip)
+    end
+  end
+
+  def destroy
+
+    Trip.destroy(params[:id])
+
+    redirect_to trips_path
+  end
+
+  private
+  def trip_params
+    return params.require(:trip).permit(:passenger_id, :driver_id)
+  end
 end

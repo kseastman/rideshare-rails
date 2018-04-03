77d1fa7dde545475447d371d77b0abc941a59718
Kat <kat.eastman@gmail.com>
2018-04-02 17:05:07 -0700

    Updated trips routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,16 @@ Rails.application.routes.draw do
 
   get 'passengers/index'
 
-  get 'rides/index'
+  get 'trips/index'
+
+  get '/trips', to: 'trips#index', as: 'trips'
+  get '/trips/new', to: 'trips#new', as: 'new_trip'
+  post '/trips', to: 'trips#create'
+
+  get '/trips/:id', to: 'trips#show', as: 'trip'
+  get '/trips/:id/edit', to: 'trips#edit', as: 'edit_trip'
+  patch '/trips/:id', to: 'trips#update'
+  delete '/trips/:id', to: 'trips#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
